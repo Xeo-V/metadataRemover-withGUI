9bd9c05fa7eea353db2ce4d2ed430ff4acb01916
X30 <74484800+Xeo-V@users.noreply.github.com>
2023-10-29 17:34:45 +0530

    Update remover.py

--- remover.py
+++ remover.py
@@ -1,1 +1,7 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QAction, QMessageBox, QTextEdit
+from PyQt5.QtCore import Qt
+from PIL import Image, ExifTags
+import datetime
+import os
 
